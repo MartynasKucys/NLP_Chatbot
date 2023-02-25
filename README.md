4cb033b2477364806f5e39a66a55627a2cf7abca
MartynasKucys <martynas.kucys@gmail.com>
2023-02-26 00:04:39 +0200

    model training

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NLP_Chatbot
+
+data set https://www.kaggle.com/code/atinsaki/chatbot-using-given-responses/data
---
+++ nlp_utils.py
@@ -0,0 +1,42 @@
+import spacy
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+
+def tokenize_to_vec(text, nlp_model):
+    doc = nlp_model(text)
+
+    useful_tokens = []
+
+    for token in doc:
+        # if not (token.is_punct and token.is_stop):
+        if not (token.is_punct):
+            useful_tokens.append(token.lemma_)
+
+    doc = nlp_model(" ".join(useful_tokens))
+
+    return doc.vector
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+
+        out = self.l2(out)
+        out = self.relu(out)
+
+        out = self.l3(out)
+
+        return out
+
